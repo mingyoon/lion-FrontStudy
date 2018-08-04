174953ab60573283913064f31acaa2d840e5cc3c
Ming <eclair305@gmail.com>
2018-08-04 12:16:48 +0000

    mobile css

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,7 +2,26 @@
 
 $(document).ready(function(){
     $('.gridBox').click(function(){
-        $('.black-background').fadeIn();
-        $('.modalElement').fadeIn();
+        var index = $('.gridBox').index(this) + 1;
+        $('.modalElement img').attr('src','box'+index+'.jpg');
+        $('.black-background, .modalElement').fadeIn();
     });
+    $('.black-background, .modalElement').click(function(){
+        $('.black-background, .modalElement').fadeOut();
+    });
+    
+    $('.mobileMenuBtn').click(function(){
+        $('.m-background').fadeIn();
+        $('.mobileMenu').animate({
+            left: 0
+        }, 500);
+    });
+    $('.m-background').click(function(){
+        $('.m-background').fadeOut();
+        $('.mobileMenu').animate({
+            left: '-100%'
+        }, 500);
+    });
+
 });
+
